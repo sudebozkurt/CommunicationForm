0e2a7c9d11cebaaeacdfd66ce016d31ff33f6780
sudebozkurt <114679744+sudebozkurt@users.noreply.github.com>
2023-09-19 22:28:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta http-equiv="Content-Language" content="tr">
 <meta charset="utf-8">
-<title>Extra Eğitim</title>
+<title>Document/title>
 <style>
 	.InputAlani{
 		width: 100%;
